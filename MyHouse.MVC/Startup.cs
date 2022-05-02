016d39ff57b332de4975b1406f44b10130be035b
ngotungdat1997@gmail.com <ngotungdat1997@gmail.com>
2022-05-03 00:09:21 +0700

    Update code

--- MyHouse.MVC/Startup.cs
+++ MyHouse.MVC/Startup.cs
@@ -29,7 +29,7 @@ namespace MyHouse.MVC
             //In-Memory
             services.AddDistributedMemoryCache();
             services.AddSession(options => {
-                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.IdleTimeout = TimeSpan.FromHours(8);
                 //options.Cookie.HttpOnly = true;
                 //options.Cookie.IsEssential = true;
             });
